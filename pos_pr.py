ac19bc051e067ef15bd2c0b14a56376543b4dcf1
Marcelo Vanti <vanti.furb@gmail.com>
2014-10-09 14:55:57 -0300

    Create pos_pr.py

---
+++ pos_pr.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 08 12014
+
+@author: MV
+"""
+import sys
+import desenha_malha as ds
+import matplotlib.tri as tri
+import matplotlib.pyplot as plt
+
+try:
+    infilename1=sys.argv[1];infilename2=sys.argv[2]
+except:
+    print "Usage:", sys.argv[0], "infile outfile"; sys.exit(1)
+
+#arquivo .malha
+m = ds.Mesh(infilename1)
+
+# arquivo de resultados
+ifile=open(infilename2,'r')
+
+v=[]
+lines=ifile.readlines()
+
+for line in lines:
+    v.append(float(line))
+
+x = []
+y = []
+z = []
+for n in m.nodes:
+    x.append(n.x)
+    y.append(n.y)
+    z.append(v[n.i-1])
+
+m.plotMesh()    
+tri.Triangulation(x, y)
+plt.tricontour(x, y, z, 20, linewidths=0.5)
+plt.tricontourf(x, y, z, 20, alpha=1,cmap=plt.cm.coolwarm)
+plt.title('EquiPotential Plot')
+plt.colorbar()
+plt.show()
